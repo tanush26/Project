e9918c77e866717cd580fab4da09b7ca94f3e394
“srilatha24” <darsanamsrilatha@gmail.com>
2019-07-02 23:59:31 +0100

    gitt

--- Desktop/gitt/src/test/java/com/ittraiing/selenium/page_objects/TrolleyPage.java
+++
@@ -1,7 +0,0 @@
-package com.ittraiing.selenium.page_objects;
-
-import com.ittraiing.selenium.Hooks;
-import com.ittraiing.selenium.driver.DriverFactory;
-
-public class TrolleyPage extends DriverFactory {
-}
